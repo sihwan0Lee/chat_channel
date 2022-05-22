fd44e2a07c1e57ad36355c9369ce36f0e9c22498
sihwan0Lee <stay136@gmail.com>
2022-05-22 19:30:03 +0900

    make consumer

--- chatwithU/ChatApp/views.py
+++ chatwithU/ChatApp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def index(request):
-    return render(request, 'ChatApp/index.html')
+    return render(request, 'ChatApp/index.html', {})
+
+
+def room(request, room_name):
+    return render(request, 'ChatApp/room.html', {
+        'room_name': room_name
+    })
--- chatwithU/chatwithU/asgi.py
+++ chatwithU/chatwithU/asgi.py
@@ -9,12 +9,18 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+import ChatApp.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatwithU.settings.develop')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            ChatApp.routing.websocket_urlpatterns
+        )
+    ),
 })
